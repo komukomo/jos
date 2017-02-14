baeadeb21ed407105f4f1d73abdffafa23fbfbb7
komukomo <komukomo@users.noreply.github.com>
2017-02-14 17:43:53 +0000

    Implement round-robin scheduling (Lab4 Ex6)

--- kern/syscall.c
+++ kern/syscall.c
@@ -281,6 +281,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return sys_env_destroy(a1);
 	case SYS_getenvid:
 		return sys_getenvid();
+	case SYS_yield:
+		sys_yield();
 	default:
 		return -E_INVAL;
 	}
