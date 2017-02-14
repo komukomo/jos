404b4f578b92370c965589f4379d36835bc33a7d
komukomo <komukomo@users.noreply.github.com>
2017-02-14 17:15:24 +0000

    Implement MMIO mapping (Lab4 Ex1)

--- kern/pmap.c
+++ kern/pmap.c
@@ -584,7 +584,11 @@ mmio_map_region(physaddr_t pa, size_t size)
 	// Hint: The staff solution uses boot_map_region.
 	//
 	// Your code here:
-	panic("mmio_map_region not implemented");
+	size = ROUNDUP(size, PGSIZE);
+	uintptr_t tmp = base;
+	boot_map_region(kern_pgdir, base, size, pa, PTE_PCD | PTE_PWT | PTE_W);
+	base += size;
+	return (void *)tmp;
 }
 
 static uintptr_t user_mem_check_addr;
