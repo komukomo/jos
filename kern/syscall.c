5a623498dd59bfc2f6d992bccb8fe463ad1b75dd
komukomo <komukomo@users.noreply.github.com>
2017-02-16 18:11:39 +0000

    Add syscall for setting the page fault handler (Lab4 Ex8)

--- kern/syscall.c
+++ kern/syscall.c
@@ -136,7 +136,13 @@ static int
 sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
 	// LAB 4: Your code here.
-	panic("sys_env_set_pgfault_upcall not implemented");
+	struct Env *e;
+	int r;
+	if ((r = envid2env(envid, &e, 1)) < 0) {
+		return r;
+	}
+	e->env_pgfault_upcall = func;
+	return 0;
 }
 
 // Allocate a page of memory and map it at 'va' with permission
@@ -358,6 +364,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return sys_page_map(a1, (void *)a2, a3, (void *)a4, a5);
 	case SYS_page_unmap:
 		return sys_page_unmap(a1, (void *)a2);
+	case SYS_env_set_pgfault_upcall:
+		return sys_env_set_pgfault_upcall(a1, (void *)a2);
 	default:
 		return -E_INVAL;
 	}
