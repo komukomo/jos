a605289376631c610d8ec30aeebe48fc53513ab9
komukomo <komukomo@users.noreply.github.com>
2017-02-17 19:45:51 +0000

    Disable interrupts in the kernel mode

--- kern/trap.c
+++ kern/trap.c
@@ -78,8 +78,8 @@ trap_init(void)
 	for (i = 0; i < 256; i++) {
 		SETGATE(idt[i], 0, GD_KT, handlers[i], 0);
 	}
-	SETGATE(idt[T_BRKPT], 1, GD_KT, handlers[T_BRKPT], 3);
-	SETGATE(idt[T_SYSCALL], 1, GD_KT, handlers[T_SYSCALL], 3);
+	SETGATE(idt[T_BRKPT], 0, GD_KT, handlers[T_BRKPT], 3);
+	SETGATE(idt[T_SYSCALL], 0, GD_KT, handlers[T_SYSCALL], 3);
 
 	// Per-CPU setup 
 	trap_init_percpu();
