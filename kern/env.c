c9e8daa30b47c2c74da4f87b5284d0b127f890f5
komukomo <komukomo@users.noreply.github.com>
2017-02-21 02:02:12 +0000

    Set env_type (Lab3 ?)

--- kern/env.c
+++ kern/env.c
@@ -391,6 +391,7 @@ env_create(uint8_t *binary, enum EnvType type)
 		panic("env_create: env_alloc failed");
 	}
 	load_icode(e ,binary);
+	e->env_type = type;
 	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
 	// LAB 5: Your code here.
 	if (type == ENV_TYPE_FS)
