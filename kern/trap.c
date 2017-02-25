40f88507e87607c5b6f46c5b5565f0e65259022b
komukomo <komukomo@users.noreply.github.com>
2017-02-25 14:53:35 +0000

    Increment 'ticks' for every clock interrupt (Lab6 Ex1)

--- kern/trap.c
+++ kern/trap.c
@@ -216,6 +216,8 @@ trap_dispatch(struct Trapframe *tf)
 	// LAB 4: Your code here.
 	if (tf->tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
 		lapic_eoi();
+		if (cpunum() == 0)
+			time_tick();
 		sched_yield();
 		return;
 	}
