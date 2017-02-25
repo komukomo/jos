019ca1133faf123402cc2157a2990ca66db2d6f1
komukomo <komukomo@users.noreply.github.com>
2017-02-25 11:25:21 +0000

    [bugfix] Return NULL if there is no page mapped

--- kern/pmap.c
+++ kern/pmap.c
@@ -511,7 +511,7 @@ page_lookup(pde_t *pgdir, void *va, pte_t **pte_store)
 {
 	// Fill this function in
 	pte_t *pte = pgdir_walk(pgdir, va, 0);
-	if (!pte) return NULL;
+	if (!pte || !(*pte & PTE_P)) return NULL;
 	if (pte_store)
 		*pte_store = pte;
 
