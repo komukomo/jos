a438a40f6c3b600cf1aff4db80ca55da4bf67e9d
komukomo <komukomo@users.noreply.github.com>
2017-02-27 07:01:25 +0000

    Initialize E1000 (Lab6 Ex3)

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,10 @@
 #include <kern/e1000.h>
 
 // LAB 6: Your driver code here
+#include <kern/pci.h>
+
+int
+e1000_attachfn(struct pci_func *pcif) {
+	pci_func_enable(pcif);
+	return 0; // What shoud I return?
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,12 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include <kern/pci.h>
+
+#define VENDOR_ID_82540EM 0x8086
+#define DEVICE_ID_82540EM 0x100e
+
+
+int e1000_attachfn(struct pci_func *pcif);
+
 #endif	// JOS_KERN_E1000_H
