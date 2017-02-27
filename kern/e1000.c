7698b4c5a63007bc2709d1dda39597ce6a2c87df
komukomo <komukomo@users.noreply.github.com>
2017-02-27 09:51:32 +0000

    Create mapping for BAR 0 (Lab6 Ex4)

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,9 +2,17 @@
 
 // LAB 6: Your driver code here
 #include <kern/pci.h>
+#include <kern/pmap.h>
+
+volatile uint32_t *reg;
 
 int
 e1000_attachfn(struct pci_func *pcif) {
 	pci_func_enable(pcif);
+	reg = mmio_map_region((physaddr_t)pcif->reg_base[0],
+			pcif->reg_size[0]);
+
+	cprintf("device status: %x\n", reg[2]);
+
 	return 0; // What shoud I return?
 }
