d8861fc4d222c5d4cb9d81b89ccae3318900784c
komukomo <komukomo@users.noreply.github.com>
2017-03-02 11:54:19 +0000

    Implement a input function for the core network server (Lab6 Ex12)

--- net/input.c
+++ net/input.c
@@ -1,6 +1,22 @@
 #include "ns.h"
+#include "inc/lib.h"
 
 extern union Nsipc nsipcbuf;
+
+void
+sleep(int msec)
+{
+	unsigned now = sys_time_msec();
+	unsigned end = now + msec;
+
+	if ((int)now < 0 && (int)now > -MAXERROR)
+		panic("sys_time_msec: %e", (int)now);
+	if (end < now)
+		panic("sleep: wrap");
+
+	while (sys_time_msec() < end)
+		sys_yield();
+}
 
 void
 input(envid_t ns_envid)
@@ -13,4 +29,16 @@ input(envid_t ns_envid)
 	// Hint: When you IPC a page to the network server, it will be
 	// reading from it for a while, so don't immediately receive
 	// another packet in to the same physical page.
+	size_t size;
+	char buf[1518];
+	while(1) {
+		if (sys_pkt_recv(buf, &size) < 0) {
+			continue;
+		}
+		memcpy(nsipcbuf.pkt.jp_data, buf, size);
+		nsipcbuf.pkt.jp_len = size;
+		ipc_send(ns_envid, NSREQ_INPUT, &nsipcbuf, PTE_P|PTE_W|PTE_U);
+		sleep(50);
+	}
 }
+
